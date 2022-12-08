99b2574227d5b752629f21c310ec5bf41b488d87
DouglasDiones <douglas.marcelino@ufv.br>
2022-12-08 10:46:25 -0300

    Commit inicial

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "produtoCartesiano.h"
+void main(){
+    
+}
+
+
+/*a,b,c,d
+aa,ab,ac,ad,ba,bb,bc,bd,ca,cb,cc,cd,da,db,dc,dd
+aaa,aab,aac,aad,aba,abb,abc,abd,aca,acb,acc,acd,ada,adb,adc,add*/
